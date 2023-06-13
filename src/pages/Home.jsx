a9e5a9cb5fb1e017d55d0b55d8c0162b97517714
Abhishek <abhi885990@gmail.com>
2023-06-13 15:16:32 +0530

    added update feature

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,13 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import Navbar from "../components/Navbar";
 import { BiMessageSquareAdd, BiMessageSquareX } from "react-icons/bi";
 import { db } from "../firebase/FirebaseAuth";
-import { QuerySnapshot } from "firebase/firestore";
 import { useSelector } from "react-redux";
+import Button from "@mui/material/Button";
+import Modal from "@mui/material/Modal";
+import TextField from "@mui/material/TextField";
+import Slide from "@mui/material/Slide";
+import Box from "@mui/material/Box";
 import {
   getFirestore,
   collection,
@@ -15,15 +19,41 @@ import {
   updateDoc,
   setDoc,
 } from "firebase/firestore";
-import { MdArchive, MdUnarchive } from "react-icons/md";
+import { MdArchive, MdUnarchive, MdOutlineEdit } from "react-icons/md";
 import { AiOutlineEye } from "react-icons/ai";
+const Transition = React.forwardRef(function Transition(props, ref) {
+  return <Slide direction="up" ref={ref} {...props} />;
+});
 function Home() {
   const [addNote, setAddNote] = useState(false);
   const user = useSelector((state) => state.user);
-
   const [items, setItems] = useState([]);
   const [msg, setMsg] = useState("");
+  const [open, setOpen] = React.useState(false);
+  const [selectedItem, setSelectedItem] = useState({ id: 123, title: "" });
+  const valueRef = useRef();
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
 
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handleSelectedItem = (item) => {
+    setSelectedItem(item);
+  };
+  const style = {
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    width: 370,
+    bgcolor: "background.paper",
+    border: "2px solid #000",
+    boxShadow: 24,
+    p: 4,
+  };
   const fetchItems = async () => {
     const db = getFirestore();
     const itemsCollectionRef = collection(db, "users", user.user.uid, "items");
@@ -41,6 +71,10 @@ function Home() {
     }
   };
   const createItem = async () => {
+    if (msg.length === 0) {
+      setAddNote(!addNote);
+      return;
+    }
     const newItem = { id: Date.now(), title: msg, archived: false };
     const db = getFirestore();
     const itemsCollectionRef = collection(db, "users", user.user.uid, "items");
@@ -54,11 +88,36 @@ function Home() {
       console.error("Error adding item:", error);
     }
   };
+  const updateItem = async () => {
+    const id = selectedItem.id.toString();
+    const itemRef = doc(getFirestore(), "users", user.user.uid, "items", id);
+
+    try {
+      await updateDoc(itemRef, { title: valueRef.current.value });
+      console.log("Item updated successfully.");
+      fetchItems();
+      handleClose();
+    } catch (error) {
+      console.error("Error updating item:", error);
+    }
+  };
+  const archiveItem = async (itemId) => {
+    const id = itemId.toString();
+    const itemRef = doc(db, "users", user.user.uid, "items", id);
+
+    try {
+      await updateDoc(itemRef, { archived: true });
+      console.log("Item archived successfully.");
+      fetchItems();
+    } catch (error) {
+      console.error("Error archiving item:", error);
+    }
+  };
 
   useEffect(() => {
     fetchItems();
   }, []);
-
+  console.log("selected item", selectedItem);
   return (
     <>
       <Navbar />
@@ -107,15 +166,30 @@ function Home() {
                   key={item.id}
                   className="w-full cursor-pointer hover:scale-105 duration-500 min-h-[4rem] h-auto  flex justify-start items-center p-2 rounded-md border-2 border-gray-500 shadow-md hover:bg-white hover:text-black "
                 >
-                  <div className="w-[78%] md:w-[88%] lg:w-[92%] mr-2">
+                  <div className="w-[72%] md:w-[88%] lg:w-[90%] mr-3">
                     <p className="truncate  hover:text-clip hover:whitespace-normal ">
                       {" "}
                       {item.title}
                     </p>
                   </div>
-                  <div className="flex justify-center items-center gap-2 md:gap-4 ">
-                    <MdArchive size={22} />
-                    <AiOutlineEye size={22} />
+                  {/* <div className="w-[72%] md:w-[88%] lg:w-[90%] mr-3">
+                    <input
+                      type="text"
+                      className="truncate  hover:text-clip hover:whitespace-normal bg-transparent border-none h-full"
+                      value={item.title}
+                      onChange={(e) => console.log(e.target.value)}
+                    />
+                  </div> */}
+                  <div className="flex justify-center items-center gap-2 ">
+                    <MdOutlineEdit
+                      size={20}
+                      onClick={() => {
+                        handleSelectedItem(item);
+                        handleClickOpen();
+                      }}
+                    />
+                    <MdArchive size={20} onClick={() => archiveItem(item.id)} />
+                    <AiOutlineEye size={20} />
                   </div>
                 </div>
               );
@@ -123,6 +197,40 @@ function Home() {
           </div>
         </div>
       </div>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <TextField
+            id="outlined-multiline-static"
+            defaultValue={selectedItem.title}
+            multiline
+            rows={4}
+            inputRef={valueRef}
+            style={{ width: 300, marginBottom: "1rem" }}
+          />
+          <Button
+            variant="contained"
+            color="success"
+            rows={4}
+            style={{ margin: "1rem 1rem 0 0" }}
+            onClick={updateItem}
+          >
+            Update
+          </Button>
+          <Button
+            variant="contained"
+            color="error"
+            style={{ margin: "1rem 1rem 0 0" }}
+            onClick={handleClose}
+          >
+            Cancel
+          </Button>
+        </Box>
+      </Modal>
     </>
   );
 }
