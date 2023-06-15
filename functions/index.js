2b6d1e8a92e6ae22f5a82459169a77c9e4d4045f
Abhishek <abhi885990@gmail.com>
2023-06-15 12:17:12 +0530

    added firebase functions

---
+++ functions/index.js
@@ -0,0 +1,8 @@
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+
+admin.initializeApp();
+
+exports.newUserTrigger = functions.auth.user().onCreate(async (user) => {
+  functions.logger.log("A new user signed in");
+});
